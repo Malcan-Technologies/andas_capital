2e6fca3272b60fd6f1e09bff057ef60f0cf65849
Ivancky42 <ivan.chewky@gmail.com>
2026-02-09 12:14:11 +0800

    Update Content-Security-Policy in next.config.js for admin and frontend
    - Removed 'unsafe-eval' from the script-src directive in the Content-Security-Policy for both admin and frontend configurations to enhance security.

--- admin/next.config.js
+++ admin/next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
 					},
 					{
 						key: "Content-Security-Policy",
-						value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://api.andas.com.my; frame-ancestors 'none'; base-uri 'self'; object-src 'none'",
+						value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://api.andas.com.my; frame-ancestors 'none'; base-uri 'self'; object-src 'none'",
 					},
 					{
 						key: "Referrer-Policy",
